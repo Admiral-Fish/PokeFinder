0cb1dec2707ed3819fef01b71dc00df9be2fcad0
pkmnbumba <pkmnbumba@gmail.com>
2017-06-20 19:54:08 -0700

    adding tests

---
+++ Test/LCRNG/LCRNG.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+    LCRNG.cpp \
+    LCRNGTest.cpp
+
+HEADERS += \
+    LCRNG.hpp
---
+++ Test/LCRNG/LCRNGTest.cpp
@@ -0,0 +1,20 @@
+#include "LCRNG.hpp"
+#include <iostream>
+
+int main () {
+    LCRNG test = LCRNG();
+    test.setpokeRNG();
+    std::cout << std::hex << test.getSeed() << std::endl;
+    std::cout << std::hex << test.next32Bit() << std::endl;
+    std::cout << std::hex << test.next32Bit() << std::endl;
+    std::cout << std::hex << test.next32Bit() << std::endl;
+    std::cout << std::hex << test.next32Bit() << std::endl;
+    std:: cout << std::endl;
+    uint32_t seed = 0;
+    for(int i = 0;i<5;i++) {
+        seed = (seed*0x41C64E6D + 0x00006073)&0xFFFFFFFF;
+        std::cout << std::hex << seed << std::endl;
+    }
+
+
+}
