8a0a56f0e06e86fda4757bc75e5f77408856362c
EzPzStreamz <steviecook210@gmail.com>
2017-09-27 23:24:18 -0400

    Redone table filling

--- Core/Gen3/FrameGen3.cpp
+++ Core/Gen3/FrameGen3.cpp
@@ -55,3 +55,28 @@ void FrameGen3::SetIVsManual(uint32_t iv1, uint32_t iv2, uint32_t iv3, uint32_t 
     hidden = ((((hp & 1) + 2 * (atk & 1) + 4 * (def & 1) + 8 * (spe & 1) + 16 * (spa & 1) + 32 * (spd & 1)) * 15) / 63);
     power = (30 + ((((hp >> 1) & 1) + 2 * ((atk >> 1) & 1) + 4 * ((def >> 1) & 1) + 8 * ((spe >> 1) & 1) + 16 * ((spa >> 1) & 1) + 32 * ((spd >> 1) & 1)) * 40 / 63));
 }
+
+QList<QStandardItem *> FrameGen3::GetTableRow()
+{
+    QList<QStandardItem *> row;
+    row.append(new QStandardItem(QString::number(frame)));
+    row.append(new QStandardItem(GetTime()));
+    row.append(new QStandardItem(QString::number(pid, 16).toUpper().rightJustified(8,'0')));
+    row.append(new QStandardItem(GetShiny()));
+    row.append(new QStandardItem(GetNature()));
+    row.append(new QStandardItem(QString::number(ability)));
+    row.append(new QStandardItem(QString::number(hp)));
+    row.append(new QStandardItem(QString::number(atk)));
+    row.append(new QStandardItem(QString::number(def)));
+    row.append(new QStandardItem(QString::number(spa)));
+    row.append(new QStandardItem(QString::number(spd)));
+    row.append(new QStandardItem(QString::number(spe)));
+    row.append(new QStandardItem(GetPower()));
+    row.append(new QStandardItem(QString::number(power)));
+    row.append(new QStandardItem(GetFemale125()));
+    row.append(new QStandardItem(GetFemale25()));
+    row.append(new QStandardItem(GetFemale50()));
+    row.append(new QStandardItem(GetFemale75()));
+
+    return row;
+}
