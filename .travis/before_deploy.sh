f72410d0753e4f1685eac695fdf0d163f8b9f4f3
Zak Kurka <zakkurka@gmail.com>
2017-11-13 07:36:48 -0600

    Added languages to Linux and Mac artifacts

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = linux ]; then
-    zip PokeFinder-linux.zip PokeFinder
+    zip PokeFinder-linux.zip PokeFinder languages
 else
-    /usr/local/Cellar/qt/5.9.1/bin/macdeployqt PokeFinder.app -dmg -verbose=2
-    mv PokeFinder.dmg PokeFinder-macOS.dmg
+    zip -r PokeFinder-macOS.zip PokeFinder.app languages
 fi
