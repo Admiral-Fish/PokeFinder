7042c2c26c4c0e7a2ae2f23e61afee8f3a6ff188
Admiral-Fish <admiralfish420@gmail.com>
2017-11-25 17:42:24 -0700

    Readd QT packaging for macOS

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -3,5 +3,6 @@
 if [ $TRAVIS_OS_NAME = linux ]; then
     zip PokeFinder-linux.zip PokeFinder languages
 else
-    zip -r PokeFinder-macOS.zip PokeFinder.app languages
+	/usr/local/Cellar/qt/5.9.1/bin/macdeployqt PokeFinder.app -dmg -verbose=2
+    zip -r PokeFinder-macOS.zip PokeFinder.dmg languages
 fi
