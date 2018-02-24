bc62186e6e967056718124f5620a4ef18ef4c65d
Admiral-Fish <admiralfish420@gmail.com>
2018-02-23 21:52:55 -0700

    Set style

--- main.cpp
+++ main.cpp
@@ -19,10 +19,13 @@
 
 #include "Forms/MainWindow.hpp"
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
+
     MainWindow w;
     w.show();
 
