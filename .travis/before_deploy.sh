2fa62436c17ae5efb70bf4314bb8fff9e4f8d485
Admiral-Fish <admiralfish420@gmail.com>
2018-02-26 22:06:49 -0700

    Spacing

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm Languages/*.ts
 if [ $TRAVIS_OS_NAME = linux ]; then
-    zip -r PokeFinder-linux.zip PokeFinder Languages
+	zip -r PokeFinder-linux.zip PokeFinder Languages
 else
 	/usr/local/Cellar/qt/5.10.1/bin/macdeployqt PokeFinder.app -verbose=2
 	mkdir PokeFinder.app/Contents/MacOS/Languages
