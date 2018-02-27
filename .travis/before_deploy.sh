e48a2ec6693fee3fa6977bbebbaf65f19718059a
Admiral-Fish <admiralfish420@gmail.com>
2018-02-26 21:09:33 -0800

    CI tweaks (#38)
    * Package Mac better
    
    * Fix directories
    
    * Spacing
    
    * Change back appveyor branch

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 rm Languages/*.ts
 if [ $TRAVIS_OS_NAME = linux ]; then
-    zip -r PokeFinder-linux.zip PokeFinder Languages
+	zip -r PokeFinder-linux.zip PokeFinder Languages
 else
-    /usr/local/Cellar/qt/5.10.0/bin/macdeployqt PokeFinder.app -dmg -verbose=2
-    zip -r PokeFinder-macOS.zip PokeFinder.dmg Languages
+	/usr/local/Cellar/qt/5.10.1/bin/macdeployqt PokeFinder.app -verbose=2
+	mkdir PokeFinder.app/Contents/MacOS/Languages
+	cp Languages/*.qm PokeFinder.app/Contents/MacOS/Languages
+	zip -r PokeFinder-macOS.zip PokeFinder.app
 fi
