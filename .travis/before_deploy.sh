49cd1477eae3f0a1f70a043ecd7e1ac2ffa0ba7f
Admiral-Fish <admiralfish420@gmail.com>
2018-03-05 13:01:17 -0800

    Travis test (#39)

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -3,8 +3,8 @@ rm Languages/*.ts
 if [ $TRAVIS_OS_NAME = linux ]; then
 	zip -r PokeFinder-linux.zip PokeFinder Languages
 else
-	/usr/local/Cellar/qt/5.10.1/bin/macdeployqt PokeFinder.app -verbose=2
 	mkdir PokeFinder.app/Contents/MacOS/Languages
 	cp Languages/*.qm PokeFinder.app/Contents/MacOS/Languages
-	zip -r PokeFinder-macOS.zip PokeFinder.app
+	/usr/local/Cellar/qt/5.10.1/bin/macdeployqt PokeFinder.app -dmg -verbose=2
+	zip -r PokeFinder-macOS.zip PokeFinder.dmg
 fi
