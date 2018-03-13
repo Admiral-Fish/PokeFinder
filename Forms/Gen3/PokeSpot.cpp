c1ad4ebaa013bdbeb347fd4353e486fb66baf3a0
Admiral-Fish <admiralfish420@gmail.com>
2018-03-12 17:54:13 -0700

    Switch to pointers

--- Forms/Gen3/PokeSpot.cpp
+++ Forms/Gen3/PokeSpot.cpp
@@ -26,6 +26,7 @@ PokeSpot::PokeSpot(QWidget *parent) :
 {
     ui->setupUi(this);
     setAttribute(Qt::WA_QuitOnClose, false);
+    setAttribute(Qt::WA_DeleteOnClose);
     setWindowFlags(Qt::Widget | Qt::MSWindowsFixedSizeDialogHint);
 
     setupModels();
--- Forms/Util/IVtoPID.cpp
+++ Forms/Util/IVtoPID.cpp
@@ -26,6 +26,7 @@ IVtoPID::IVtoPID(QWidget *parent) :
 {
     ui->setupUi(this);
     setAttribute(Qt::WA_QuitOnClose, false);
+    setAttribute(Qt::WA_DeleteOnClose);
 
     setupModels();
 
