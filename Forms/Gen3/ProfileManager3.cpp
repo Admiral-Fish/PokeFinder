050ed3af70269c85684956234c325d52e14bb3b4
Admiral-Fish <admiralfish420@gmail.com>
2018-07-30 22:24:52 -0700

    Gen 4 UI tweaking

--- Forms/Gen3/ProfileManager3.cpp
+++ Forms/Gen3/ProfileManager3.cpp
@@ -27,6 +27,7 @@ ProfileManager3::ProfileManager3(QWidget *parent) :
     ui->setupUi(this);
     setAttribute(Qt::WA_QuitOnClose, false);
     setAttribute(Qt::WA_DeleteOnClose);
+    setWindowFlags(Qt::Widget | Qt::MSWindowsFixedSizeDialogHint);
 
     setupModels();
 }
