9daeee1ae7e81ab344a40870dc5e95a5564f2e34
Admiral-Fish <admiralfish420@gmail.com>
2018-08-08 21:11:47 -0700

    CI tweaks

--- .travis/before_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm Languages/*.ts
-mkdir PokeFinder.app/Contents/MacOS/Languages
-cp Languages/*.qm PokeFinder.app/Contents/MacOS/Languages
-/usr/local/Cellar/qt/5.11.1/bin/macdeployqt PokeFinder.app -dmg -verbose=2
-zip -r PokeFinder-macOS.zip PokeFinder.dmg
--- .travis/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-brew update
-brew install qt5
-brew link --force qt5
