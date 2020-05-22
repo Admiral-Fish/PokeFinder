cdeceb58cb2e1e7a04e8d239771878e6e869f6b4
Admiral-Fish <admiralfish420@gmail.com>
2020-05-22 16:56:44 -0700

    Add sorting

--- Models/Gen5/EventModel5.cpp
+++ Models/Gen5/EventModel5.cpp
@@ -97,7 +97,93 @@ EventSearcherModel5::EventSearcherModel5(QObject *parent) : TableModel<EventFram
 
 void EventSearcherModel5::sort(int column, Qt::SortOrder order)
 {
-    // TODO
+    if (!model.empty())
+    {
+        emit layoutAboutToBeChanged();
+        bool flag = order == Qt::AscendingOrder;
+        switch (column)
+        {
+        case 0:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getInitialSeed() < frame2.getInitialSeed() : frame1.getInitialSeed() > frame2.getInitialSeed();
+            });
+            break;
+        case 1:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getFrame() < frame2.getFrame().getFrame()
+                            : frame1.getFrame().getFrame() > frame2.getFrame().getFrame();
+            });
+            break;
+        case 2:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getPID() < frame2.getFrame().getPID()
+                            : frame1.getFrame().getPID() > frame2.getFrame().getPID();
+            });
+            break;
+        case 3:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getShiny() < frame2.getFrame().getShiny()
+                            : frame1.getFrame().getShiny() > frame2.getFrame().getShiny();
+            });
+            break;
+        case 4:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getNature() < frame2.getFrame().getNature()
+                            : frame1.getFrame().getNature() > frame2.getFrame().getNature();
+            });
+            break;
+        case 5:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getAbility() < frame2.getFrame().getAbility()
+                            : frame1.getFrame().getAbility() > frame2.getFrame().getAbility();
+            });
+            break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            std::sort(model.begin(), model.end(), [flag, column](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getIV(static_cast<u8>(column - 6)) < frame2.getFrame().getIV(static_cast<u8>(column - 6))
+                            : frame1.getFrame().getIV(static_cast<u8>(column - 6)) > frame2.getFrame().getIV(static_cast<u8>(column - 6));
+            });
+            break;
+        case 12:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getHidden() < frame2.getFrame().getHidden()
+                            : frame1.getFrame().getHidden() > frame2.getFrame().getHidden();
+            });
+            break;
+        case 13:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getPower() < frame2.getFrame().getPower()
+                            : frame1.getFrame().getPower() > frame2.getFrame().getPower();
+            });
+            break;
+        case 14:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getFrame().getGender() < frame2.getFrame().getGender()
+                            : frame1.getFrame().getGender() > frame2.getFrame().getGender();
+            });
+            break;
+        case 15:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getDateTime() < frame2.getDateTime() : frame1.getDateTime() > frame2.getDateTime();
+            });
+            break;
+        case 16:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getTimer0() < frame2.getTimer0() : frame1.getTimer0() > frame2.getTimer0();
+            });
+            break;
+        case 17:
+            std::sort(model.begin(), model.end(), [flag](const EventFrame5 &frame1, const EventFrame5 &frame2) {
+                return flag ? frame1.getButtons() < frame2.getButtons() : frame1.getButtons() > frame2.getButtons();
+            });
+            break;
+        }
+    }
 }
 
 int EventSearcherModel5::columnCount(const QModelIndex &parent) const
