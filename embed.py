45ff72eb2ba9f25dd6a125dafcfec89f516312f4
Admiral-Fish <admiralfish420@gmail.com>
2020-11-22 19:01:43 -0700

    Force python3

--- embed.py
+++ embed.py
@@ -4,14 +4,16 @@ import glob
 def embed(paths):    
     arrays = []
     for path in paths:
-        files = glob.glob(path + "/**/*.bin", recursive=True)
+        files = glob.glob(f"{path}/**/*.bin", recursive=True)
         for file in files:
             with open(file, "rb") as f:
                 data = f.read()
 
             name = os.path.basename(f.name).replace(".bin", "")
             
-            string = "constexpr u8 " + name + "[" + str(len(data)) + "] = {"
+            string = f"constexpr u8 {name}[{len(data)}] = "
+            string += " { "
+
             for i in range(len(data)):
                 string += str(data[i])
                 if i != len(data) - 1:
@@ -24,10 +26,11 @@ def embed(paths):
 
 def write(arrays):
     f = open("Core\Resources\Resources.hpp", "w")
+
     f.write("#include <Core/Util/Global.hpp>\n\n")
 
     for array in arrays:
-        f.write(array + "\n\n")
+        f.write(f"{array}\n\n")
 
 def main():
     arrays = embed(["Core\Resources\Encounters", "Core\Resources\Personal"])
