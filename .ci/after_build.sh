5c8ebf028f91829855108e5f867d48919b642a13
Admiral-Fish <admiralfish420@gmail.com>
2020-11-30 10:54:43 -0700

    Remove from CI

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,12 +3,11 @@
 case $OS in
   linux)
   {
-    zip -r PokeFinder-linux.zip PokeFinder Core/Resources/i18n
+    zip -r PokeFinder-linux.zip PokeFinder
     sha256sum PokeFinder-linux.zip > PokeFinder-linux.zip.sha256
   } ;;  
   macOS)
   {
-	mv Core/Resources/i18n PokeFinder.app/Contents/MacOS/i18n
     macdeployqt PokeFinder.app -dmg -verbose=2
     zip -r PokeFinder-macOS.zip PokeFinder.dmg
     shasum -a 256 PokeFinder-macOS.zip > PokeFinder-macOS.zip.sha256
