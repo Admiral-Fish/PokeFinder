98461e16e1999fe3b42d9134ee35c93c4a86fbff
Admiral-Fish <admiralfish420@gmail.com>
2020-12-19 14:42:17 -0700

    Change Qt path

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-	
+
 case $OS in
   linux)
   {
-    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE ../ -DCMAKE_PREFIX_PATH=/home/appveyor/Qt/5.14.2/gcc_64/bin
+    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE ../ -DCMAKE_PREFIX_PATH=/home/appveyor/Qt/5.14.2/gcc_64
   } ;;  
   macOS)
   {
