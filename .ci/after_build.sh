3fc10b26cd60c72f5b2697a87d78d41a5467e82c
Admiral-Fish <admiralfish420@gmail.com>
2021-02-21 21:32:56 -0700

    Tweak path

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,12 +3,12 @@
 case $OS in
   linux)
   {
-    zip -r PokeFinder-linux.zip PokeFinder
+    zip -r PokeFinder-linux.zip Source/Forms/PokeFinder
     sha256sum PokeFinder-linux.zip > PokeFinder-linux.zip.sha256
   } ;;  
   macOS)
   {
-    macdeployqt PokeFinder.app -dmg -verbose=2
+    macdeployqt Source/Forms/PokeFinder.app -dmg -verbose=2
     zip -r PokeFinder-macOS.zip PokeFinder.dmg
     shasum -a 256 PokeFinder-macOS.zip > PokeFinder-macOS.zip.sha256
   } ;;
