88ea622f2111e344a38887523b70d26070cc5a4f
Admiral-Fish <admiralfish420@gmail.com>
2021-02-27 12:20:34 -0700

    Tweak build/artifact paths

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,13 +3,15 @@
 case $OS in
   linux)
   {
-    zip -r PokeFinder-linux.zip Source/Forms/PokeFinder
+    mv build/Source/Forms/PokeFinder .
+    zip -r PokeFinder-linux.zip PokeFinder
     sha256sum PokeFinder-linux.zip > PokeFinder-linux.zip.sha256
   } ;;  
   macOS)
   {
-    macdeployqt Source/Forms/PokeFinder.app -dmg -verbose=2
-    zip -r PokeFinder-macOS.zip Source/Forms/PokeFinder.dmg
+    mv build/Source/Forms/PokeFinder.app .
+    macdeployqt PokeFinder.app -dmg -verbose=2
+    zip -r PokeFinder-macOS.zip PokeFinder.dmg
     shasum -a 256 PokeFinder-macOS.zip > PokeFinder-macOS.zip.sha256
   } ;;
 esac
