bf8f26d5a4cdd5c8e92858230cbf2474f04cceb2
Admiral-Fish <admiralfish420@gmail.com>
2021-04-05 14:23:28 -0700

    Make tests optional

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,10 +3,10 @@
 case $OS in
   linux)
   {
-    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE ../ -DCMAKE_PREFIX_PATH=/home/appveyor/Qt/5.14.2/gcc_64
+    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=/home/appveyor/Qt/5.14.2/gcc_64 -DTEST=ON ../ 
   } ;;  
   macOS)
   {
-	PATH="$(brew --prefix qt5)/bin:$PATH" cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE ../
+	PATH="$(brew --prefix qt5)/bin:$PATH" cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DTEST=ON ../
   } ;;
 esac
