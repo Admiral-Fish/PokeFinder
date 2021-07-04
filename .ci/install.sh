1dd4bf470af941693091f61407c70fc084ec74f2
Admiral-Fish <admiralfish420@gmail.com>
2021-07-04 12:16:16 -0700

    Tweak Mac build
    Use pre-imaged Qt instead of brew

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,10 +6,4 @@ case $OS in
     sudo sudo apt-get update
     sudo apt-get install -y libgl1-mesa-dev
   } ;;
-  macOS)
-  {
-    brew update
-    brew install qt5
-    brew link --force qt5
-  } ;;
 esac
