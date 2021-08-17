794b5dcaf5e3abc026128dc0a1d96be7ba9ea741
SteveCookTU <steviecook210@gmail.com>
2021-08-16 21:01:51 -0400

    Added poketch calculator to the gen 4 egg generator context menu (#171)
    * Added poketch calculator to the gen 4 egg generator context menu
    
    * Fix Poketch Calculator note label
    
    * Poketch fixes
    
    Hide when not using DPPt
    label fixes
    
    * Update profiles after setup
    
    * Formatting changes
    
    * Whitespace fix
    
    * Fixed profile and method check for visibility

---
+++ Source/Forms/Gen4/Tools/Poketch.cpp
@@ -0,0 +1,51 @@
+#include "Poketch.hpp"
+#include "ui_Poketch.h"
+#include <Core/Util/Utilities.hpp>
+
+Poketch::Poketch(u32 advances, QWidget *parent) : QWidget(parent), ui(new Ui::Poketch)
+{
+    ui->setupUi(this);
+    setAttribute(Qt::WA_QuitOnClose, false);
+    setAttribute(Qt::WA_DeleteOnClose);
+
+    setupModels(advances);
+}
+
+void Poketch::setupModels(u32 advances)
+{
+    connect(ui->pushButtonClose, &QPushButton::clicked, this, &Poketch::on_pushButtonClose_clicked);
+
+    u32 coin = 0;
+    u32 happy = 0;
+    u32 target = 0;
+
+    if (advances < 12)
+    {
+        coin = advances;
+        ui->labelNote->setText(tr("Do not switch to the happiness application at all"));
+    }
+    else
+    {
+        target = advances - 12;
+        happy = target / 12;
+        coin = target % 12;
+
+        if (happy == 0)
+        {
+            ui->labelNote->setText(tr("Switch to the happiness application once but do not click"));
+        }
+    }
+
+    ui->labelHappyResult->setText(QString::number(happy));
+    ui->labelCoinFlipsResult->setText(QString::number(coin));
+}
+
+Poketch::~Poketch()
+{
+    delete ui;
+}
+
+void Poketch::on_pushButtonClose_clicked()
+{
+    this->close();
+}
