90c4e981246f8384c3f1dce18290274b35856704
Shiny Sylveon <26980015+ShinySylveon04@users.noreply.github.com>
2021-09-21 20:16:41 -0500

    refactoring

--- Source/Core/Gen5/Generators/IDGenerator5.cpp
+++ Source/Core/Gen5/Generators/IDGenerator5.cpp
@@ -50,24 +50,14 @@ std::vector<IDState5> IDGenerator5::generate(u64 seed, u32 pid, bool checkPID, b
 
         if (filter.compare(state))
         {
-            if (checkPID)
-            {
-                bool idBit = (tid & 1) ^ (sid & 1);
+            bool idBit = (tid & 1) ^ (sid & 1);
 
-                // Check if PID will be modified by TID/SID combo if XOR box is checked
-                if (checkXOR && (xorPSV >> 3) == state.getTSV())
-                {
-                    state.setSeed(seed);
-                    states.emplace_back(state);
-                }
+            // Check if PID will be modified by TID/SID combo if XOR box is checked
+            u16 actualPSV = (idBit ^ pidBit) ? xorPSV : psv;
+            bool isShinyXor = checkXOR && (actualPSV >> 3) == state.getTSV();
+            bool isShiny = !checkXOR && (psv >> 3) == state.getTSV();
 
-                if (!(idBit ^ pidBit) && (psv >> 3) == state.getTSV())
-                {
-                    state.setSeed(seed);
-                    states.emplace_back(state);
-                }
-            }
-            else
+            if (!checkPID || isShinyXor || isShiny)
             {
                 state.setSeed(seed);
                 states.emplace_back(state);
