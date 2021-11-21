2adcc165b7d7e84f0968f9c4969c371733c4006f
Admiral-Fish <admiralfish420@gmail.com>
2021-11-21 00:25:43 -0700

    Switch windows to jom

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,10 +3,12 @@
 case $OS in
   linux)
   {
-    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=$HOME/Qt/6.1/gcc_64 -DTEST=ON ../ 
+    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=$HOME/Qt/6.1/gcc_64 -DTEST=ON ../
+    make -j $(nproc)
   } ;;  
   macOS)
   {
-	  PATH=$PATH:$HOME/Qt/6.1/macos/bin cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DTEST=ON ../
+    PATH=$PATH:$HOME/Qt/6.1/macos/bin cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DTEST=ON ../
+    make -j $(sysctl -n hw.physicalcpu)
   } ;;
 esac
