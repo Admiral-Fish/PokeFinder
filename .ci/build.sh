42a30cc14da8eba7d1d5b0db3b2db96a9780f5b8
Admiral-Fish <admiralfish420@gmail.com>
2021-11-21 10:56:00 -0700

    Revert CI changes

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,12 +3,10 @@
 case $OS in
   linux)
   {
-    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=$HOME/Qt/6.1/gcc_64 -DTEST=ON ../
-    make -j $(nproc)
+    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=$HOME/Qt/6.1/gcc_64 -DTEST=ON ../ 
   } ;;  
   macOS)
   {
     PATH=$PATH:$HOME/Qt/6.1/macos/bin cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DTEST=ON ../
-    make -j $(sysctl -n hw.physicalcpu)
   } ;;
 esac
