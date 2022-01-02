33db5628c0de4ff96f7682c4393f32f9ec8d3f6b
Admiral-Fish <admiralfish420@gmail.com>
2022-01-01 22:23:07 -0700

    Tweak mac deployment

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -10,22 +10,11 @@ case $OS in
   macOS)
   {
     mv build/Source/Forms/PokeFinder.app .
-    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -verbose=3
+    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -no-plugins -verbose=3
 
-    # Remove libraries we don't need
-    rm -r PokeFinder.app/Contents/Frameworks/QtDBus.framework
-    rm -r PokeFinder.app/Contents/Frameworks/QtOpenGL.framework
-    rm -r PokeFinder.app/Contents/Frameworks/QtQml.framework
-    rm -r PokeFinder.app/Contents/Frameworks/QtQmlModels.framework
-    rm -r PokeFinder.app/Contents/Frameworks/QtQuick.framework
-    rm -r PokeFinder.app/Contents/Frameworks/QtSvg.framework
-    rm -r PokeFinder.app/Contents/Frameworks/QtVirtualKeyboard.framework
-
-    # Remove plugins we don't need
-    rm -r PokeFinder.app/Contents/PlugIns/iconengines
-    rm -r PokeFinder.app/Contents/PlugIns/imageformats
-    rm -r PokeFinder.app/Contents/PlugIns/platforminputcontexts
-    rm -r PokeFinder.app/Contents/PlugIns/virtualkeyboard
+    # Copy over plugins we need
+    cp $HOME/Qt/6.1/macos/plugins/platforms/libqcococa.dylib PokeFinder.app/Contents/PlugIns/platforms
+    cp $HOME/Qt/6.1/macos/plugins/styles/libqmacstyle.dylib PokeFinder.app/Contents/PlugIns/styles
 
     tar czf PokeFinder-macOS.tar.gz PokeFinder.app
     shasum -a 256 PokeFinder-macOS.tar.gz > PokeFinder-macOS.tar.gz.sha256
