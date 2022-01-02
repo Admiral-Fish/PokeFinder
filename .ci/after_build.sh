cea62866f99fce5b040484ac5792d88292c4d192
Admiral-Fish <admiralfish420@gmail.com>
2022-01-01 22:11:24 -0700

    Tweak deployment

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -4,14 +4,30 @@ case $OS in
   linux)
   {
     mv build/Source/Forms/PokeFinder .
-    zip -r PokeFinder-linux.zip PokeFinder
-    sha256sum PokeFinder-linux.zip > PokeFinder-linux.zip.sha256
+    tar czf PokeFinder-linux.tar.gz PokeFinder
+    sha256sum PokeFinder-linux.tar.gz > PokeFinder-linux.tar.gz.sha256
   } ;;  
   macOS)
   {
     mv build/Source/Forms/PokeFinder.app .
-    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -dmg -verbose=2
-    zip -r PokeFinder-macOS.zip PokeFinder.dmg
-    shasum -a 256 PokeFinder-macOS.zip > PokeFinder-macOS.zip.sha256
+    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -verbose=3
+
+    # Remove libraries we don't need
+    rm -r PokeFinder.app/Contents/Frameworks/QtDBus.framework
+    rm -r PokeFinder.app/Contents/Frameworks/QtOpenGL.framework
+    rm -r PokeFinder.app/Contents/Frameworks/QtQml.framework
+    rm -r PokeFinder.app/Contents/Frameworks/QtQmlModels.framework
+    rm -r PokeFinder.app/Contents/Frameworks/QtQuick.framework
+    rm -r PokeFinder.app/Contents/Frameworks/QtSvg.framework
+    rm -r PokeFinder.app/Contents/Frameworks/QtVirtualKeyboard.framework
+
+    # Remove plugins we don't need
+    rm -r PokeFinder.app/Contents/PlugIns/iconengines
+    rm -r PokeFinder.app/Contents/PlugIns/imageformats
+    rm -r PokeFinder.app/Contents/PlugIns/platforminputcontexts
+    rm -r PokeFinder.app/Contents/PlugIns/virtualkeyboard
+
+    tar czf PokeFinder-macOS.tar.gz PokeFinder.app
+    shasum -a 256 PokeFinder-macOS.tar.gz > PokeFinder-macOS.tar.gz.sha256
   } ;;
 esac
