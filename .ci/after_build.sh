72eedb984db816c78db8b55a70f881ece786c951
Admiral-Fish <admiralfish420@gmail.com>
2022-01-04 18:53:54 -0700

    Deployment (#218)
    * Tweak deployment
    
    * Tweak mac deployment
    
    * Create qt.conf
    
    * Update artifact paths
    
    * SSH
    
    * Seperate out extension
    
    * Remove extra extension
    
    * Fix plugin deployment
    
    * Fix rpath

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -4,14 +4,27 @@ case $OS in
   linux)
   {
     mv build/Source/Forms/PokeFinder .
-    zip -r PokeFinder-linux.zip PokeFinder
-    sha256sum PokeFinder-linux.zip > PokeFinder-linux.zip.sha256
-  } ;;  
+    tar czf PokeFinder-linux.tar.gz PokeFinder
+    sha256sum PokeFinder-linux.tar.gz > PokeFinder-linux.tar.gz.sha256
+  } ;;
   macOS)
   {
     mv build/Source/Forms/PokeFinder.app .
-    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -dmg -verbose=2
-    zip -r PokeFinder-macOS.zip PokeFinder.dmg
-    shasum -a 256 PokeFinder-macOS.zip > PokeFinder-macOS.zip.sha256
+    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -no-plugins -verbose=3
+
+    # Fix rpath
+    install_name_tool -rpath /Users/appveyor/Qt/6.1/macos/lib @executable_path/../Frameworks PokeFinder.app/Contents/MacOS/PokeFinder
+
+    # Create plugins folder
+    mkdir PokeFinder.app/Contents/PlugIns
+    mkdir PokeFinder.app/Contents/PlugIns/platforms
+    mkdir PokeFinder.app/Contents/PlugIns/styles
+
+    # Copy over plugins we need
+    cp $HOME/Qt/6.1/macos/plugins/platforms/libqcocoa.dylib PokeFinder.app/Contents/PlugIns/platforms
+    cp $HOME/Qt/6.1/macos/plugins/styles/libqmacstyle.dylib PokeFinder.app/Contents/PlugIns/styles
+
+    tar czf PokeFinder-macOS.tar.gz PokeFinder.app
+    shasum -a 256 PokeFinder-macOS.tar.gz > PokeFinder-macOS.tar.gz.sha256
   } ;;
 esac
--- .ci/build.sh
+++ .ci/build.sh
@@ -3,8 +3,8 @@
 case $OS in
   linux)
   {
-    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=$HOME/Qt/6.1/gcc_64 -DTEST=ON ../ 
-  } ;;  
+    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH=$HOME/Qt/6.1/gcc_64 -DTEST=ON ../
+  } ;;
   macOS)
   {
     PATH=$PATH:$HOME/Qt/6.1/macos/bin cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DTEST=ON ../
