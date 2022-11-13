2ad109ba18a1d492127a8dee787f87700a697be3
Admiral-Fish <admiralfish420@gmail.com>
2022-11-13 11:57:48 -0700

    Optimizations

--- Source/Form/Gen8/Underground.cpp
+++ Source/Form/Gen8/Underground.cpp
@@ -1,6 +1,25 @@
+/*
+ * This file is part of PokéFinder
+ * Copyright (C) 2017-2022 by Admiral_Fish, bumba, and EzPzStreamz
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
 #include "Underground.hpp"
 #include "ui_Underground.h"
-
 #include <Core/Enum/Encounter.hpp>
 #include <Core/Enum/Lead.hpp>
 #include <Core/Gen8/EncounterArea8.hpp>
@@ -10,9 +29,9 @@
 #include <Core/Gen8/Profile8.hpp>
 #include <Core/Parents/ProfileLoader.hpp>
 #include <Core/Util/Translator.hpp>
+#include <Form/Controls/Controls.hpp>
 #include <Form/Gen8/Profile/ProfileManager8.hpp>
 #include <Model/Gen8/UndergroundModel.hpp>
-#include <QMenu>
 #include <QMessageBox>
 #include <QSettings>
 
@@ -38,6 +57,8 @@ Underground::Underground(QWidget *parent) : QWidget(parent), ui(new Ui::Undergro
     ui->toolButtonLead->addAction(tr("Vital Spirit"), toInt(Lead::VitalSpirit));
 
     ui->comboBoxLocation->setup({ 196, 199, 189, 185, 198, 184, 191, 192, 186, 193, 183, 197, 194, 195, 188, 200, 187, 190 });
+
+    ui->filter->disableControls(Controls::EncounterSlots);
 
     connect(ui->comboBoxProfiles, &QComboBox::currentIndexChanged, this, &Underground::profileIndexChanged);
     connect(ui->pushButtonGenerate, &QPushButton::clicked, this, &Underground::generate);
@@ -132,7 +153,6 @@ void Underground::generate()
     UndergroundStateFilter filter(ui->filter->getGender(), ui->filter->getAbility(), ui->filter->getShiny(),
                                   ui->filter->getDisableFilters(), ui->filter->getMinIVs(), ui->filter->getMaxIVs(),
                                   ui->filter->getNatures(), ui->filter->getHiddenPowers(), species);
-
     UndergroundGenerator generator(initialAdvances, maxAdvances, offset, tid, sid, currentProfile->getVersion(), lead, randMarkId,
                                    storyFlag, bonus, filter);
 
