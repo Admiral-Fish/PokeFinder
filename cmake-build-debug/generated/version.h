cdd620f525d0ca96ad711d11d82b22d97e99158d
Guillaume Cisco <guillaumecisco@gmail.com>
2022-12-07 16:09:12 +0100

    remove debug files

--- cmake-build-debug/generated/version.h
+++
@@ -1,8 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define POKEFINDER_VERSION "4.0.1"
-#define GIT_BRANCH "master"
-#define GIT_COMMIT "188371ce1ff6e046fd98081c8d08ddea434fbd89"
-
-#endif // VERSION_H
