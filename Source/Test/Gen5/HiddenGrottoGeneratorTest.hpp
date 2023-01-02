6987da8cb9a604e7027167a44895da5b18c8d8ce
Admiral-Fish <admiralfish420@gmail.com>
2023-01-02 08:59:46 -0700

    Update copyright year

--- Source/Test/Gen5/HiddenGrottoGeneratorTest.hpp
+++ Source/Test/Gen5/HiddenGrottoGeneratorTest.hpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of PokéFinder
- * Copyright (C) 2017-2022 by Admiral_Fish, bumba, and EzPzStreamz
+ * Copyright (C) 2017-2023 by Admiral_Fish, bumba, and EzPzStreamz
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
