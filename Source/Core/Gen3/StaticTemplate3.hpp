1e122a76049465a80eb8e3b9be87aeb6d95cb71d
Real96 <chicco.gaeta@gmail.com>
2024-05-15 01:22:35 +0200

    Requested changes

--- Source/Core/Gen3/StaticTemplate3.hpp
+++ Source/Core/Gen3/StaticTemplate3.hpp
@@ -36,10 +36,9 @@ public:
      * @param form Form of the template
      * @param shiny Shiny state of the template
      * @param level Level of the template
-     * @param buggedRoamer FRLG/RS roamer
      */
-    constexpr StaticTemplate3(Game version, u16 specie, u8 form, Shiny shiny, u8 ability, u8 gender, u8 ivCount, u8 level) :
-        StaticTemplate(version, specie, form, shiny, ability, gender, ivCount, level), buggedRoamer(((specie == 380 || specie == 381)
+    constexpr StaticTemplate3(Game version, u16 specie, u8 form, Shiny shiny, u8 level) :
+        StaticTemplate(version, specie, form, shiny, 255, 255, 0, level), buggedRoamer(((specie == 380 || specie == 381)
                                                                                                      && (version & Game::RS) != Game::None)
                                                                                                     || (specie >= 243 && specie <= 245))
     {
