15e328bdcf80f73d4806a8d26cf9f767cc92bc54
NickPlayeZ <80699972+NickPlayeZ@users.noreply.github.com>
2025-12-01 23:31:46 +0100

    Update translations for 'Initial Advances' and 'Advances'

--- Form/i18n/PokeFinder_zh.ts
+++ Form/i18n/PokeFinder_zh.ts
@@ -3938,12 +3938,12 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/IDModel5.hpp" line="81"/>
-        <source>Initial Advances</source>
+        <source>Starting Advance</source>
         <translation type="unfinished">初始帧</translation>
     </message>
     <message>
         <location filename="../../Model/Gen5/IDModel5.hpp" line="81"/>
-        <source>Advances</source>
+        <source>Advance</source>
         <translation type="unfinished">帧数</translation>
     </message>
     <message>
