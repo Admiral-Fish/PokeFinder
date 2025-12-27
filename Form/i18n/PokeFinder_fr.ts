bcce3dc081ccc7e5f21c75a583a73a01082e58fb
NickPlayeZ <80699972+NickPlayeZ@users.noreply.github.com>
2025-12-27 21:50:51 +0100

    Change some searcher tabs "Advances" to "Target Advance" P1
    Changed many of the "Advances" column headers in ONLY the searcher tabs of gen 5 windows to "Target Advance" as that is a more fitting descriptor that has confused quite a lot of newcomers to gen 5 rng in the past. Also changed the gen 5 IDs Initial Advances to Starting Advance for the same reason; a more fitting descriptor. IV Advances and all Dream Radar Advances stayed as they are as those start at 0 and therefore just "Advances" is perfectly accurate.

--- Form/i18n/PokeFinder_fr.ts
+++ Form/i18n/PokeFinder_fr.ts
@@ -1027,7 +1027,7 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/EggModel5.hpp" line="155"/>
-        <source>Advances</source>
+        <source>Target Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -2629,7 +2629,7 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/EventModel5.hpp" line="140"/>
-        <source>Advances</source>
+        <source>Target Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -3713,7 +3713,7 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/HiddenGrottoModel.hpp" line="236"/>
-        <source>Advances</source>
+        <source>Target Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -3844,7 +3844,7 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/HiddenGrottoModel.hpp" line="121"/>
-        <source>Advances</source>
+        <source>Target Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -3938,12 +3938,12 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/IDModel5.hpp" line="81"/>
-        <source>Initial Advances</source>
+        <source>Starting Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../Model/Gen5/IDModel5.hpp" line="81"/>
-        <source>Advances</source>
+        <source>Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -8597,7 +8597,7 @@
     </message>
     <message>
         <location filename="../../Model/Gen5/StaticModel5.hpp" line="140"/>
-        <source>Advances</source>
+        <source>Target Advance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
