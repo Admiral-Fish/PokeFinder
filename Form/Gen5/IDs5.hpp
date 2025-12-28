576e86d12a2b2b93122e87666f1a2f872c3cad8f
Admiral-Fish <admiralfish420@gmail.com>
2025-12-28 11:58:53 -0700

    Cleanup

--- Form/Gen5/IDs5.hpp
+++ Form/Gen5/IDs5.hpp
@@ -105,16 +105,6 @@ private slots:
      * @brief Opens the profile manager
      */
     void profileManager();
-
-    /**
-     * @brief Transfers the current Searcher filters to the Generator.
-     */
-    void transferFiltersToGenerator();
-
-    /**
-     * @brief Transfers the current Searcher settings to the Generator.
-     */
-    void transferSettingsToGenerator();
 };
 
 #endif // IDS5_HPP
