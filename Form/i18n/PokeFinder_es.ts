f7bcf0a6c125703de01c5eeb796b5abeb47d9b3c
NickPlayeZ <80699972+NickPlayeZ@users.noreply.github.com>
2026-07-08 05:41:13 +0200

    [Gen 4] Searcher upgrades (#587)
    Add delay display to gen 4 searcher tabs. Add hour display to gen wild and static searchers for time based wild encounters + rotom

--- Form/i18n/PokeFinder_es.ts
+++ Form/i18n/PokeFinder_es.ts
@@ -8633,6 +8633,11 @@ Profile is missing or has an incompatible SHA cache.</source>
     </message>
     <message>
         <location filename="../../Model/Gen4/StaticModel4.hpp" line="159"/>
+        <source>Hour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Model/Gen4/StaticModel4.hpp" line="159"/>
         <source>Advances</source>
         <translation type="unfinished"></translation>
     </message>
@@ -10994,6 +10999,11 @@ Profile is missing or has an incompatible SHA cache.</source>
         <location filename="../../Model/Gen4/WildModel4.hpp" line="158"/>
         <source>Seed</source>
         <translation type="unfinished">Seed</translation>
+    </message>
+    <message>
+        <location filename="../../Model/Gen4/WildModel4.hpp" line="158"/>
+        <source>Hour</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../Model/Gen4/WildModel4.hpp" line="158"/>
--- Form/i18n/PokeFinder_ja.ts
+++ Form/i18n/PokeFinder_ja.ts
@@ -8626,6 +8626,11 @@ Profile is missing or has an incompatible SHA cache.</source>
     </message>
     <message>
         <location filename="../../Model/Gen4/StaticModel4.hpp" line="159"/>
+        <source>Hour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Model/Gen4/StaticModel4.hpp" line="159"/>
         <source>Advances</source>
         <translation type="unfinished"></translation>
     </message>
@@ -10986,6 +10991,11 @@ Profile is missing or has an incompatible SHA cache.</source>
     <message>
         <location filename="../../Model/Gen4/WildModel4.hpp" line="158"/>
         <source>Seed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Model/Gen4/WildModel4.hpp" line="158"/>
+        <source>Hour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
